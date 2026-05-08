f962d0a4ddcba383ff52bf22a592e7a222615b84
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2026-05-08 20:17:57 +0530

    Clerk Middleware for Hono

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,20 +1,22 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
+app.get("/hello", clerkMiddleware(), (c) => {
+  const auth = getAuth(c);
+  if (!auth?.userId) {
+    return c.json({
+      error: "Unauthorized",
+    });
+  }
   return c.json({
     message: "Hello Next.js!",
-  });
-});
-
-app.get("/hello/:id", (c) => {
-  const { id } = c.req.param();
-  return c.json({
-    message: `Hello ${id}`,
   });
 });
 
