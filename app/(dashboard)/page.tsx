569d204477eab7485749733fea8559507302c6e8
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2026-05-08 18:22:35 +0530

    Setup and Auth

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const page = () => {
+  return (
+    <div>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  )
+}
+
+export default page
