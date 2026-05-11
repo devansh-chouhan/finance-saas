93c0b438f72419b836ed2dd38c3cab61011d4047
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2026-05-11 18:30:26 +0530

    Categories Section Done

---
+++ drizzle/0000_groovy_mentor.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "accounts" (
+	"id" text PRIMARY KEY NOT NULL,
+	"plaid_id" text,
+	"name" text NOT NULL,
+	"user_id" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "categories" (
+	"id" text PRIMARY KEY NOT NULL,
+	"plaid_id" text,
+	"name" text NOT NULL,
+	"user_id" text NOT NULL
+);
--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -5,13 +5,9 @@ import { useMountedState } from "react-use";
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 
-// import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
-// import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
+import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
 
-// import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
-// import { EditTransactionSheet } from "@/features/transactions/components/edit-transaction-sheet";
-
-// import { SubscriptionModal } from "@/features/subscriptions/components/subscription-modal";
 
 export const SheetProvider = () => {
   const isMounted = useMountedState();
@@ -23,13 +19,9 @@ export const SheetProvider = () => {
       <NewAccountSheet />
       <EditAccountSheet />
 
-      {/* <NewCategorySheet />
+      <NewCategorySheet />
       <EditCategorySheet />
 
-      <NewTransactionSheet />
-      <EditTransactionSheet />
-
-      <SubscriptionModal /> */}
     </>
   );
 };
